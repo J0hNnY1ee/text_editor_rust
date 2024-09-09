1cdc30f8f35c0da1c6186b96e1b4723547fac447
J0hNnY1ee <leejohnny2024@gmail.com>
2024-09-09 15:41:11 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
-use std::io::{stdin, Read};
 
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+mod editor;
+use editor::Editor;
 
 
 
 fn main()
 {
-    enable_raw_mode().unwrap();
-    for b in stdin().bytes().into_iter()
-    {
-        let b = b.unwrap();   
-        let c = b as char;
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        }
-        else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-        }
-        println!("{c}");
-        if c == 'q'
-        {
-            disable_raw_mode().unwrap();
-            break;
-        }
-    }
+   let editor = Editor::default();
+   editor.run();
 }
