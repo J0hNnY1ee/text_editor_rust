971a3c6d6cc86f43c5fbd06fbbdb982fce171c2b
J0hNnY1ee <leejohnny2024@gmail.com>
2024-09-11 19:23:00 +0800

    finish assignment 8

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,23 +1,27 @@
-use crossterm::{
-    cursor::{Hide, MoveTo, Show},
-    queue,
-    style::Print,
-    terminal::{disable_raw_mode, enable_raw_mode, size, Clear},
-    Command,
-};
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::style::Print;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use crossterm::{queue, Command};
 use std::io::{stdout, Error, Write};
 
-pub struct Terminal {}
-#[derive(Clone, Copy)]
+#[derive(Default, Copy, Clone)]
 pub struct Size {
     pub height: usize,
     pub width: usize,
 }
-#[derive(Copy, Clone,Default)]
+#[derive(Copy, Clone, Default)]
 pub struct Position {
     pub col: usize,
     pub row: usize,
 }
+
+
+
+
+
+
+pub struct Terminal;
+
 impl Terminal {
     pub fn terminate() -> Result<(), Error> {
         Self::execute()?;
@@ -30,17 +34,20 @@ impl Terminal {
         Self::execute()?;
         Ok(())
     }
-
-    pub fn clear_line() -> Result<(), Error> {
-        Self::queue_command(Clear(crossterm::terminal::ClearType::CurrentLine))?;
-        Ok(())
-    }
     pub fn clear_screen() -> Result<(), Error> {
-        Self::queue_command(Clear(crossterm::terminal::ClearType::All))?;
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
-
+    pub fn clear_line() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
+        Ok(())
+    }
+    
+    
+    
     pub fn move_caret_to(position: Position) -> Result<(), Error> {
+        
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
         Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
         Ok(())
     }
@@ -56,19 +63,27 @@ impl Terminal {
         Self::queue_command(Print(string))?;
         Ok(())
     }
+
+    
+    
+    
     pub fn size() -> Result<Size, Error> {
         let (width_u16, height_u16) = size()?;
+        
+        #[allow(clippy::as_conversions)]
         let height = height_u16 as usize;
+        
+        #[allow(clippy::as_conversions)]
         let width = width_u16 as usize;
         Ok(Size { height, width })
     }
-
     pub fn execute() -> Result<(), Error> {
         stdout().flush()?;
         Ok(())
     }
-    fn queue_command<T: Command>(commond: T) -> Result<(), Error> {
-        queue!(stdout(), commond)?;
+
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
         Ok(())
     }
 }
